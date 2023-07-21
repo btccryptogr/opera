4012d502fc73a3ecffb599bd5d31d2881827e6ad
btccryptogr <140140036+btccryptogr@users.noreply.github.com>
2023-07-21 07:43:17 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xdd170f2adfcac8fff843bb9ece7c9458bf53a430";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
